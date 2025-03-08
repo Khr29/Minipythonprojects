f729edebe9d9cb2399e1f1d4ecfd3d9620acaa27
Khr29 <khaled.taha.ahmed12@gmail.com>
2025-03-08 13:38:52 +0530

    project5 done

--- project5/main_calculator.py
+++ project5/main_calculator.py
@@ -1,15 +1,21 @@
 import calculator_functions as calculator
 
+# main function to run the calculator
 def main():
     print("Welcome to the Calculator App")
 
     while True:
+        # ask for numbers and operator
         num1 = calculator.get_valid_number("Enter first number: ")
         num2 = calculator.get_valid_number("Enter second number: ")
         operator = input("Choose operation ( +, -, *, /): ").strip()
+
+        # show result
         print(f"Result: {calculator.perform_calculattion(num1, num2, operator)}")
+
+        # ask to continue or stop
         if input("Calculate again? (yes/no): ").strip().lower() != "yes":
-            print("Goodbye!")
+            print("Goodbye! 👋")
             break
 
 if __name__ == "__main__":
