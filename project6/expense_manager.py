ebb4ebce783fc31ae4ab9a6f670fd541d50053f5
Khr29 <khaled.taha.ahmed12@gmail.com>
2025-03-10 16:14:36 +0530

    GitHub User Activity new project

--- project6/expense_manager.py
+++ project6/expense_manager.py
@@ -1,5 +1,6 @@
 from expense_functions import add_expense, view_expenses, total_expenses, filter_by_category, safe_to_file, load_from_file
 
+# main function - controls the entire program
 def main():
     expenses = load_from_file()
     while True:
@@ -12,6 +13,7 @@ def main():
 
         choice = input("Choose an option: ")
 
+        # add expense - takes amount, category, and description
         if choice == "1":
             try:
                 amount = float(input("Enter the amount number: "))
@@ -22,15 +24,18 @@ def main():
             except ValueError:
                 print("Invalid amount. Please enter valid number.")
 
+        # view expenses - shows all saved expenses
         elif choice == "2":
             view_expenses(expenses)
+
         elif choice == "3":
             total_expenses(expenses)
 
+        # total expenses - shows the total amount spent
         elif choice == "4":
             category = input("Enter category to filter: ")
             filter_by_category(expenses, category)
-
+        # save and exit - saves expenses and ends the program
         elif choice == "5":
             safe_to_file(expenses)
             print("Expenses saved")
@@ -38,6 +43,7 @@ def main():
         else:
             print("invalid choice. please try again.")
 
+# run the program if this file is executed directly
 if __name__ == "__main__":
     main()
 
