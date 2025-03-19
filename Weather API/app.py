bf487fc3c37b6d125237060e191bf4cd0ece6c07
Khr29 <khaled.taha.ahmed12@gmail.com>
2025-03-19 19:49:29 +0530

    .

--- Weather API/app.py
+++ Weather API/app.py
@@ -2,3 +2,13 @@ from flask import Flask, redirect,jsonify
 import requests
 import os
 from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()
+
+API_KEY = os.getenv("Weather_api_key")
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    
